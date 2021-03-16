86e6c5d34695274a71da871607d5167735b0bf5a
prince.gupta <prince.gupta@timesinternet.in>
2021-03-16 14:41:51 +0530

    experiment of git reset commit2

--- src/main/java/com/prince/order/repository/DeliveryPersonRepository.java
+++ src/main/java/com/prince/order/repository/DeliveryPersonRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryPersonRepository extends CrudRepository<DeliveryPerson, Long> {
+
     DeliveryPerson findByMobileNo(String mobileNo);
 }
